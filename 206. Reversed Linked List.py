7f7e03684fbad6b427e2fff1158778d29de6e75d
kevinzhong666 <81261168+kevinzhong666@users.noreply.github.com>
2024-02-28 00:17:58 -0500

    good copy

--- 206. Reversed Linked List.py
+++ 206. Reversed Linked List.py
@@ -1,9 +1,10 @@
 class Solution(object):
     def reverseList(self, head):
-      current = head
-      prev = None
-      while current != None :
-          current.next = self
-          current.next = prev
-          prev = current
-          current = next
+        current = head
+        prev = None
+        while current is not None:
+            next_temp = current.next  # Temporarily store the next node
+            current.next = prev       # Reverse the current node's pointer
+            prev = current            # Move prev to current
+            current = next_temp       # Move to the next node in the original list
+        return prev  # prev is the new head of the reversed list

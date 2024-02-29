3f71a21e2a3613ae8f2c1c9372b9d2edff61881d
kevinzhong666 <81261168+kevinzhong666@users.noreply.github.com>
2024-02-29 00:01:46 -0500

    runtime is too slow

--- 20. Valid Parentheses.py
+++ 20. Valid Parentheses.py
@@ -1,7 +1,20 @@
 class Solution(object):
     def isValid(self, s):
-        bracket = []
-        bracket.append(open_bracket)
-        brackt_map
-        {"}": "{", ")": "(", "]": "["}
-
+        stack = []
+        for char in s:
+            if char == '(' or char == '[' or char == '{':
+                stack.append(char)
+            if char == ')':
+                if not stack or stack.pop() != '(':
+                    return False
+            if char == ']':
+                if not stack or stack.pop() != '[':
+                    return False
+            if char == '}':
+                if not stack or stack.pop() != '{':
+                    return False
+        # Edge for
+        # "[" or "{" or "("
+        if stack:
+            return False
+        return True

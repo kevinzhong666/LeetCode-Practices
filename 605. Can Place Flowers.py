a590d6049a6bc9b0370d25834a12659399ba3895
kevinzhong666 <81261168+kevinzhong666@users.noreply.github.com>
2024-04-03 13:50:44 -0400

    7/129 testcases passed

--- 605. Can Place Flowers.py
+++ 605. Can Place Flowers.py
@@ -2,6 +2,7 @@ class Solution(object):
     def canPlaceFlowers(self, flowerbed, n):
         for i in range(len(flowerbed)):
             if flowerbed[i] == 0 and flowerbed[i - 1] == 0 and flowerbed[i + 1] == 0:
+                flowerbed[i] = 1
                 n -= 1
                 if n == 0:
                     return True
